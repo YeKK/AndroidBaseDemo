43f75a7d45107f1e4bc3630351a9bc3a339dd511
yejian <1135510101@qq.com>
2022-05-15 19:45:35 +0800

    [modify]不同等级日志测试

--- LogTestJava/app/src/main/java/com/example/logtestjava/MainActivity.java
+++ LogTestJava/app/src/main/java/com/example/logtestjava/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.logtestjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, "onCreate: ", new Throwable());
+        Log.e(TAG, "onCreate: ");
+        Log.w(TAG, "onCreate: ", new Throwable());
+        Log.w(TAG, "onCreate: ");
+        Log.i(TAG, "onCreate: ");
+        Log.d(TAG, "onCreate: ");
+        Log.v(TAG, "onCreate: "); //筛选器会筛出比它等级更高的log,而不是等级低的log
     }
 }
